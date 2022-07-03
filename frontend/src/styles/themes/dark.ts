97d18be8e616746398edd83399428114a5b8f8fc
Joao Fernandes <joao.fernandes@impar.com.br>
2022-07-03 19:08:34 -0300

    fist commit

---
+++ frontend/src/styles/themes/dark.ts
@@ -0,0 +1,15 @@
+const light = {
+  title: 'light',
+  colors: {
+    text: '#E3E3E3',
+    backgroundColor: '#222226',
+    backgroundColorTab: '#121214',
+
+    primary: '#fC9D74',
+    secondary: '#C23A00',
+
+    default: '#9C9C9C',
+  },
+}
+
+export default light
